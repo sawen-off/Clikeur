4294608051e1d4b3623c81710d855be7e6cf505f
SAWEN <85832826+sawen-off@users.noreply.github.com>
2021-10-29 14:09:58 +0200

    Add files via upload

---
+++ Clik/compteurclik.js
@@ -0,0 +1,9 @@
+var x=-1 
+function compteur() 
+{ 
+x = x+1; 
+document.getElementById('nb').innerHTML = x; 
+} 
+  
+  
+  
---
+++ Clik/compteurespace.js
@@ -0,0 +1,6 @@
+var x=0 
+function compteur() 
+{ 
+x = x+1; 
+document.getElementById('nb').innerHTML = x; 
+} 
